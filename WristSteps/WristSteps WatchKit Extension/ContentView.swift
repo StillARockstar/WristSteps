76f8f48c83cc5bae33d86b5de062a921f694fc84
Michael Schoder <m.schoder@my-realm.net>
2020-08-30 13:33:00 +0200

    project setup

---
+++ WristSteps/WristSteps WatchKit Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WristSteps WatchKit Extension
+//
+//  Created by Michael Schoder on 30.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("WristSteps Relaunch").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WristSteps/WristSteps WatchKit Extension/WristStepsApp.swift
@@ -0,0 +1,19 @@
+//
+//  WristStepsApp.swift
+//  WristSteps WatchKit Extension
+//
+//  Created by Michael Schoder on 30.08.20.
+//
+
+import SwiftUI
+
+@main
+struct WristStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
