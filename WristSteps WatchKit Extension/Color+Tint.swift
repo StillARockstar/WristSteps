78c546d60352659a0847ac782414cb9a9e628306
StillARockstar <m.schoder@my-realm.net>
2020-09-03 22:09:11 +0200

    add app color definitions

--- WristSteps WatchKit Extension/Color+Tint.swift
+++ WristSteps WatchKit Extension/Color+Tint.swift
@@ -10,5 +10,10 @@ import SwiftUI
 extension Color {
     static let appTint = Self.appBlue
 
-    static let appBlue = Self.blue
+    static let appBlue = Color("appBlue")
+    static let appCyan = Color("appCyan")
+    static let appGreen = Color("appGreen")
+    static let appOrange = Color("appOrange")
+    static let appPurple = Color("appPurple")
+    static let appRed = Color("appRed")
 }
