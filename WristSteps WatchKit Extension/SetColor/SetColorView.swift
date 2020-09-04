e5e71545cb2ab0b0b3ec0a1877b08688ece8232d
StillARockstar <m.schoder@my-realm.net>
2020-09-04 15:56:24 +0200

    implement layout

--- WristSteps WatchKit Extension/SetColor/SetColorView.swift
+++ WristSteps WatchKit Extension/SetColor/SetColorView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct SetColorView: View {
+    let data = ["1", "2", "3", "4", "5", "6"]
+
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("App Color".uppercased())
+                .foregroundColor(.appTint)
+            Text("App & Complication color")
+                .font(.footnote)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+            Spacer()
+
+            LazyVGrid(columns: columns, spacing: 5){
+                ForEach(data, id: \.self) { item in
+                    Button(
+                        action: { },
+                        label: { Image(systemName: "checkmark.circle.fill") }
+                    )
+                        .padding()
+                        .font(.title)
+                        .foregroundColor(.blue)
+                        .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
     }
 }
 
