70f80942c4b2b7e19994c11f7a2222c020d86914
Michael Schoder <m.schoder@my-realm.net>
2021-03-08 19:38:18 +0100

    introduce InfoView

--- WristSteps WatchKit Extension/DesignSystem/InfoView.swift
+++ WristSteps WatchKit Extension/DesignSystem/InfoView.swift
@@ -7,20 +7,43 @@
 
 import SwiftUI
 
-//struct InfoViewProvider {
-//
-//}
-//
-//struct InfoView: View {
-//    let provider: InfoViewProvider
-//
-//    var body: some View {
-//        Text("Hello, World!")
-//    }
-//}
-//
-//struct InfoView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        InfoView()
-//    }
-//}
+struct InfoViewProvider {
+    let emoji: String?
+    let title: String
+    let body: String
+}
+
+struct InfoView: View {
+    let provider: InfoViewProvider
+
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                if let emoji = self.provider.emoji {
+                    Text(emoji)
+                        .font(.largeTitle)
+                }
+                Text(self.provider.title)
+                    .font(.headline)
+                    .foregroundColor(.appTint)
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Text(self.provider.body)
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+        }
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(
+            provider: InfoViewProvider(
+                emoji: "👋",
+                title: "Info View Title",
+                body: "This is the body is of an info view! It is scollable. And it is awesome and my be super long"
+            )
+        )
+    }
+}
