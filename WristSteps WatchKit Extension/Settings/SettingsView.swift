d057cd0cd42b21b70b42fa1c9b2f788047e0f9bd
Michael Schoder <m.schoder@my-realm.net>
2021-03-17 21:21:20 +0100

    implement SettingsView Layout

--- WristSteps WatchKit Extension/Settings/SettingsView.swift
+++ WristSteps WatchKit Extension/Settings/SettingsView.swift
@@ -11,7 +11,33 @@ struct SettingsView: View {
     let provider: SettingsViewProvider
 
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack {
+                SettingsButton(emoji: "🏁", label: "Goal")
+                SettingsButton(emoji: "🎨", label: "Color")
+                SettingsButton(emoji: "👋", label: "Help")
+                SettingsButton(emoji: "ℹ️", label: "About App")
+            }
+        }
+    }
+}
+
+private struct SettingsButton: View {
+    let emoji: String
+    let label: String
+
+    var body: some View {
+        Button(
+            action: {},
+            label: {
+                HStack(spacing: 10) {
+                    Text(emoji)
+                    Text(label)
+                    Spacer()
+                }
+                .padding(.leading, 4)
+            }
+        )
     }
 }
 
