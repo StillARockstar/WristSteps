fdbaee7982ac3445ec15a1be6c77d000847b1cbd
StillARockstar <56514572+StillARockstar@users.noreply.github.com>
2021-05-26 21:50:59 +0200

    fix: Debug File scrolling (#35)
    * add scrollview
    
    * remove padding

--- WristSteps WatchKit Extension/DebugMenu/DebugMenuFilesView.swift
+++ WristSteps WatchKit Extension/DebugMenu/DebugMenuFilesView.swift
@@ -11,7 +11,7 @@ struct DebugMenuFilesView: View {
     let provider: DebugMenuViewProvider
 
     var body: some View {
-        VStack {
+        ScrollView {
             ForEach(provider.files, id: \.self, content: { filename in
                 NavigationLink(
                     destination: DebugMenuFileView(content: provider.content(of: filename)),
@@ -20,7 +20,6 @@ struct DebugMenuFilesView: View {
                             BodyText(filename, alignment: .leading)
                             Body1Text(provider.lastChanged(of: filename), alignment: .leading)
                         }
-                        .padding(.leading, 10)
                     })
             })
             Spacer()
