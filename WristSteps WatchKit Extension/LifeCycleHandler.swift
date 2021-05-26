32ab1bfe5f2cc869ab54ea32c98378c5770c19e0
StillARockstar <56514572+StillARockstar@users.noreply.github.com>
2021-05-26 21:20:04 +0200

    fix: Midnight Step Reset (#33)
    * do bulk update after midnight
    
    * load previous hour on background update

--- WristSteps WatchKit Extension/LifeCycleHandler.swift
+++ WristSteps WatchKit Extension/LifeCycleHandler.swift
@@ -55,8 +55,11 @@ private extension LifeCycleHandler {
             return
         }
 
-        WKExtension.shared().scheduleBackgroundRefresh(withPreferredDate: updateDate, userInfo: nil, scheduledCompletion: { error in
-            completion(error == nil)
+        WKExtension.shared().scheduleBackgroundRefresh(
+            withPreferredDate: updateDate,
+            userInfo: nil,
+            scheduledCompletion: { error in
+                completion(error == nil)
         })
     }
 
@@ -69,12 +72,25 @@ private extension LifeCycleHandler {
             sema.wait()
         }
         let operation2 = BlockOperation { [weak self] in
-            let sema = DispatchSemaphore(value: 0)
             let hour = Calendar.current.component(.hour, from: Date())
-            self?.dataProvider.healthData.updateHour(hour: hour, completion: {
-                sema.signal()
-            })
-            sema.wait()
+            if hour == 0 {
+                let sema = DispatchSemaphore(value: 0)
+                self?.dataProvider.healthData.updateBulk(completion: {
+                    sema.signal()
+                })
+                sema.wait()
+            } else {
+                let sema0 = DispatchSemaphore(value: 0)
+                let sema1 = DispatchSemaphore(value: 0)
+                self?.dataProvider.healthData.updateHour(hour: hour - 1, completion: {
+                    sema0.signal()
+                })
+                self?.dataProvider.healthData.updateHour(hour: hour, completion: {
+                    sema1.signal()
+                })
+                sema0.wait()
+                sema1.wait()
+            }
         }
 
         let loadingQueue = OperationQueue()
